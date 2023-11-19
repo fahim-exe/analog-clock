632bf9b2027f2ced56124bba3ae4eb63ed485bf1
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-11-19 23:00:52 +0600

    need some update has bugs

---
+++ app.js
@@ -0,0 +1,27 @@
+const secondBar = document.querySelector(".second-bar");
+const minuteBar = document.querySelector(".minute-bar");
+const hourBar = document.querySelector(".hour-bar");
+
+
+function setTime(){
+    const presentTime = new Date();
+
+    const timeSecond = presentTime.getSeconds();
+    const secDegrees = ((timeSecond/60)*360)+90;
+    secondBar.style.transform = `rotate(${secDegrees}deg)`;
+
+    const timeMinute = presentTime.getMinutes();    
+    const minDegrees = ((timeMinute / 60) * 360) + ((timeSecond /60) * 6) + 90;
+    minuteBar.style.transform = `rotate(${minDegrees})deg`;
+
+
+    const timeHour = presentTime.getHours();
+    const hourDegrees = ((timeHour / 12) * 360) + ((timeMinute /60) * 30) + 90;
+    hourBar.style.transform = `rotate(${hourDegrees})deg`;
+
+    
+}
+
+setInterval(setTime, 1000);
+
+setTime();
